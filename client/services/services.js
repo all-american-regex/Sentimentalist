00460baf456ccc8c7b78d9c90e7c887c8a11cd6b
MKS-students <mks.students@hackreactor.com>
2016-04-01 15:19:40 -0500

    can grab sentiment score and political leaning

--- client/services/services.js
+++ client/services/services.js
@@ -13,19 +13,63 @@ angular.module('sL.services', [])
       })
     };
 
-    var updateScores = function(/*what goes here?*/){
+    var updateScore = function(datum){
       return $http({
         method: 'GET',
         url: '/api/scrapearticle',
         params:{
-          url:
+          url: datum.url
         }
       })
+    };
+
+    var averageScore = function(scoresObject){
+      var temp = 0
+      for (var i = 0; i < scoresObject.data.sentiment.length;i++){
+        temp=temp+scoresObject.data.sentiment[i];
+      }
+      var result= temp/scoresObject.data.sentiment.length
+      result = Math.floor(result*100)
+      return result;
     }
+
+    var politicalSide = function(politicalScores){
+      var temp = {
+        Conservative: 0,
+        Green: 0,
+        Liberal: 0,
+        Libertarian: 0
+      }
+
+      for (var i = 0; i < politicalScores.data.political.length;i++){
+        temp.Conservative+=politicalScores.data.political[i].Conservative;
+        temp.Green +=politicalScores.data.political[i].Green;
+        temp.Liberal += politicalScores.data.political[i].Liberal;
+        temp.Libertarian += politicalScores.data.political[i].Libertarian;
+      }
+
+      var strongestPolitical = {
+        party: "",
+        score: 0
+      }
+      for (var key in temp){
+        temp[key]=temp[key]/politicalScores.data.political.length;
+        if (temp[key] > strongestPolitical.score){
+          strongestPolitical.view=key;
+          strongestPolitical.score = temp[key];
+        }
+      }
+      console.log("PJPJPJPJPJ",strongestPolitical.score)
+      return strongestPolitical.view;
+    }
+
+
 
     return {
       getTopTen: getTopTen,
-      updateScores: updateScores
+      updateScore: updateScore,
+      averageScore: averageScore,
+      politicalSide: politicalSide
     };
   })
   .service('Data', function() {
--- client/src/searchBar.js
+++ client/src/searchBar.js
@@ -7,21 +7,48 @@ angular.module('sL.searchBar', [])
     $scope.input = "";
 
     $scope.getLinks = function() {
+       Data.newsLinks.data=[];
       News.getTopTen($scope.input).then(function(result) {
         var resultArray = [];
         for (var i = 0; i < result.data.length; i++) {
           if (result.data[i].url && result.data[i].summary) {
+<<<<<<< HEAD
             result.data[i].headline = result.data[i].summary.split('.')[0];
             var getImage = result.data[i].url.split('/')[0] + '//' + result.data[i].url.split('/')[2];
             console.log(getImage)
             result.data[i].img = getImage + '/favicon.ico';
             console.log(getImage)
+=======
+            result.data[i].score = null;
+            result.data[i].political = null;
+            result.data[i].emotion = null;
+            result.data[i].personality=null;
+>>>>>>> can grab sentiment score and political leaning
             resultArray.push(result.data[i])
           }
         }
 
         Data.newsLinks.data = resultArray;
 
+<<<<<<< HEAD
+=======
+       Data.newsLinks.data.forEach(function(datum){
+         News.updateScore(datum).then(function(scores){
+           datum.score=News.averageScore(scores);
+           datum.political=News.politicalSide(scores);
+
+            console.log('PJ',scores,"AVERAGE SCORE:",datum.score);
+
+          })
+
+        })
+>>>>>>> can grab sentiment score and political leaning
       })
-    }
-  });
+     //  .then(function(){
+     //    Data.newsLinks.data.forEach(function(datum){
+     //    console.log(datum);
+     //  })
+     // })
+
+  }
+});
