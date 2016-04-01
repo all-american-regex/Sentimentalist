8dd7e2084d079fb8e4eef3a06e7b7c0b8aacd5de
Harry Hough <harryhough@Harrys-MacBook-Pro.local>
2016-04-01 09:17:14 -0500

    Fixed bug with Ajax Call to top10scrape

--- client/src/searchBar.js
+++ client/src/searchBar.js
@@ -1,18 +1,17 @@
 'use strict';
 
 angular.module('sL.searchBar', [])
-  .controller('SearchBar', ['$scope', function($scope, News, Data) {
+  .controller('SearchBar', function($scope, News, Data) {
     $scope.appName = 'SentimentaList';
     $scope.buttonText = 'Go';
     $scope.input = "";
 
 
     $scope.getLinks = function() {
-          console.log("searchBar controller >>>>", $scope.input)
-      News.getTopTen($scope.input)
-          console.log("searchBar controller >>>>", $scope.input)
-        .then(function(result) {
-          Data.newsLinks = result;
-        })
+      News.getTopTen($scope.input).then(function(result) {
+        console.log(result)
+        Data.newsLinks = result;
+      })
     }
-  }]);
+
+  });
