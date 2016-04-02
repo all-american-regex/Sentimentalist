c712d97bd3db3610d7c5d9476dc124641263ecff
John Jacob <jacobjohnrobert@gmail.com>
2016-04-01 19:32:26 -0500

    Run server in separate tab, then run 'gulp'; auto-refresh on change

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,42 +1,23 @@
 'use strict';
 
-var gulp      = require('gulp'),
-    nodemon   = require('gulp-nodemon'),
-    bs        = require('browser-sync'),
-    reload    = bs.reload,
-    when      = require('gulp-if'),
-    shell     = require('gulp-shell');
-
-
-// the paths to our app files
-var paths = {
-  // all our client app js files, not including 3rd party js files
-  scripts: ['client/app/*.js'],
-  html: ['client/index.html'],
-  styles: ['client/styles/index.css']
-};
-
-// any changes made to your
-// client side code will automagically refresh your page
-// with the new changes
-gulp.task('start', ['serve'],function () {
-  bs({
-    notify: true,
-    // address for server,
-    injectChanges: true,
-    files: paths.scripts.concat(paths.html, paths.styles),
-    proxy: 'localhost:8000'
-  });
-});
-
-gulp.task('karma', shell.task([
-  'karma start'
-]));
-
+var gulp      = require('gulp');
+var nodemon   = require('gulp-nodemon');
+var bs        = require('browser-sync').create();
+var reload    = bs.reload;
 
 // start our node server using nodemon
 gulp.task('serve', function() {
-  nodemon({script: 'server/express-server.js', ignore: 'node_modules/**/*.js'});
+  // nodemon({script: 'server/express-server.js'
+  //   // ,ext: 'js html css', 
+  //   // ignore: 'node_modules/**/*.js'
+  // });
+  bs.watch('./client/*').on('change', bs.reload);
+
+  bs.init({
+    proxy: 'localhost:3000'
+  });
+
 });
 
-gulp.task('default', ['start']);
+gulp.task('default', ['serve']);
+
