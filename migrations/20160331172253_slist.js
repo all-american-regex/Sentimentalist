bfc49465e410c4df23f638bb29c2ccff7adccada
Jake Lambert <jake@onahill.co>
2016-04-12 15:05:11 -0500

    add favorites table

--- migrations/20160331172253_slist.js
+++ migrations/20160331172253_slist.js
@@ -26,8 +26,13 @@ exports.up = function(knex, Promise) {
             table.increments('id').primary();
             table.string('query');
             table.string('sentiment');
-        })
+        }),
 
+        knex.schema.createTable('favorites', function(table){
+          table.increments('id').primary();
+          table.string('url');
+          table.integer('user_id').references('uid').inTable('users');
+        })
 
         //More elaborate results table:
 
@@ -43,7 +48,7 @@ exports.up = function(knex, Promise) {
         //          .references('id')
         //          .inTable('searches');
         // })
-        
+
         //Join table that could be implemented to find results corresponding to searches
 
         // knex.schema.createTable('search_results_join', function(table){
@@ -52,7 +57,7 @@ exports.up = function(knex, Promise) {
         //          .inTable('results');
         //     table.integer('search_id')
         //          .references('id')
-        //          .inTable('searches');       
+        //          .inTable('searches');
         // }),
     ])
 };
@@ -61,6 +66,7 @@ exports.down = function(knex, Promise) {
   return Promise.all([
         knex.schema.dropTable('users'),
         knex.schema.dropTable('searches'),
-        knex.schema.dropTable('results')
+        knex.schema.dropTable('results'),
+        knex.schema.dropTable('favorites')
     ])
 };
